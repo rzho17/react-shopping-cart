9852cbb0efca56ae230ebef561bae7575441ba12
Richard <richardzehungho@gmail.com>
2024-06-11 15:38:45 -0700

    added svgs icon for footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,39 @@
+import styles from "./Footer.module.css";
+
 export default function Footer() {
-  return <footer>Hey im the foot</footer>;
+  return (
+    <footer>
+      <div>
+        <h3>About ModishTee</h3>
+        <h3>Help</h3>
+        <h3>Account</h3>
+        <h3>E-Newsletter</h3>
+      </div>
+      <div className={styles.lower}>
+        <h2 className={styles.header}>ModishTee</h2>
+        <div className={styles.socials}>
+          <img
+            src="../src/assets/facebook.svg"
+            alt="facebook icon"
+            className={styles.icon}
+          ></img>
+          <img
+            src="../src/assets/insta.svg"
+            alt="instagram icon"
+            className={styles.icon}
+          ></img>
+          <img
+            src="../src/assets/twitter.svg"
+            alt="twitter icon"
+            className={styles.icon}
+          ></img>
+          <img
+            src="../src/assets/youtube.svg"
+            alt="youtube icon"
+            className={styles.icon}
+          ></img>
+        </div>
+      </div>
+    </footer>
+  );
 }
