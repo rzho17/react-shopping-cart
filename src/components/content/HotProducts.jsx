be1a6fe15fb001f72f16e93e5c5ac84cdcea3871
Richard <richardzehungho@gmail.com>
2024-06-21 15:10:38 -0700

    fix the loading error for the shopinfo data

--- src/components/content/HotProducts.jsx
+++ src/components/content/HotProducts.jsx
@@ -1,22 +1,57 @@
 import styles from "./HotProducts.module.css";
 import MiniProduct from "../utils/MiniProduct";
+import { useEffect, useState } from "react";
 
 export default function HotProducts() {
+  const [productData, setProductData] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // setLoading(true);
+        const data = await fetch("/data/MOCK_DATA.json");
+
+        const allData = await data.json();
+        //   console.log(allData);
+        setProductData(allData);
+        console.log("Data fetched:", allData);
+
+        // setLoading(false);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  //   console.log(productData);
+
+  //   if (setLoading) {
+  //     return <div>Loading</div>;
+  //   }
+
   return (
     <section>
       <h1>Hot Arrivals</h1>
       <div className={styles.productContainer}>
-        <MiniProduct />
-        <MiniProduct />
-        <MiniProduct />
-        <MiniProduct />
-        {/* <div className={styles.product}>
-          <img src="../src/assets/react.svg" alt="" />
-          <div className={styles.productInfo}>
-            <p>5 stars</p>
-            <p>9.99</p>
-          </div>
-        </div> */}
+        {loading === false ? (
+          productData.slice(0, 8).map((item) => {
+            return (
+              <MiniProduct
+                key={item.id}
+                img={item.image}
+                name={item.title}
+                price={item.price}
+              />
+            );
+          })
+        ) : (
+          <div>Loading Products</div>
+        )}
       </div>
     </section>
   );
--- src/components/utils/MiniProduct.jsx
+++ src/components/utils/MiniProduct.jsx
@@ -1,12 +1,13 @@
 import styles from "./MiniProduct.module.css";
 
-export default function MiniProduct({ func, img, rating, price }) {
+export default function MiniProduct({ func, img, name, price }) {
   return (
     <div className={styles.product}>
-      <img src="../src/assets/react.svg" alt="" />
+      {/* <img src="../src/assets/react.svg" alt="" onClick={func} /> */}
+      <img src={img} alt="" onClick={func} />
       <div className={styles.productInfo}>
-        <p>5 stars</p>
-        <p>9.99</p>
+        <p>{name}</p>
+        <p>{price}</p>
       </div>
     </div>
   );
