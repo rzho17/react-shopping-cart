2ab488fdc5529f6ad1b8a4e78b27808a32b3019e
Richard <richardzehungho@gmail.com>
2024-10-21 19:40:01 -0700

    add styles to product page

--- src/components/content/ProductPage.jsx
+++ src/components/content/ProductPage.jsx
@@ -1,6 +1,7 @@
 import styles from "./ProductPage.module.css";
 import Button from "../utils/Button";
 import DeliveryInfo from "../utils/DeliveryInfo";
+import SideScroller from "../utils/SideScroller";
 import { TbTruckDelivery, TbTruckReturn } from "react-icons/tb";
 import { useLocation, useOutletContext } from "react-router-dom";
 import { useState } from "react";
@@ -82,77 +83,80 @@ export default function ProductPage() {
     console.log(cartList);
   };
   return (
-    <div className={styles.productContainer}>
-      <div className={styles.imgContainer}>
-        <img src={item.mainImg} alt="" className={styles.mainImg} />
-        <div className={styles.smallImgContainer}>
-          <img
-            src={item.subImg1}
-            alt=""
-            className={`${styles.img} img1`}
-            onClick={(evt) => changeImg(evt.target.src, "img1")}
-          />
-          <img
-            src={item.subImg2}
-            alt=""
-            className={`${styles.img} img2`}
-            onClick={(evt) => changeImg(evt.target.src, "img2")}
-          />
-          <img
-            src={item.subImg3}
-            alt=""
-            className={`${styles.img} img3`}
-            onClick={(evt) => changeImg(evt.target.src, "img3")}
-          />
-          <img
-            src={item.subImg4}
-            alt=""
-            className={`${styles.img} img4`}
-            onClick={(evt) => changeImg(evt.target.src, "img4")}
-          />
+    <>
+      <div className={styles.productContainer}>
+        <div className={styles.imgContainer}>
+          <img src={item.mainImg} alt="" className={styles.mainImg} />
+          <div className={styles.smallImgContainer}>
+            <img
+              src={item.subImg1}
+              alt=""
+              className={`${styles.img} img1`}
+              onClick={(evt) => changeImg(evt.target.src, "img1")}
+            />
+            <img
+              src={item.subImg2}
+              alt=""
+              className={`${styles.img} img2`}
+              onClick={(evt) => changeImg(evt.target.src, "img2")}
+            />
+            <img
+              src={item.subImg3}
+              alt=""
+              className={`${styles.img} img3`}
+              onClick={(evt) => changeImg(evt.target.src, "img3")}
+            />
+            <img
+              src={item.subImg4}
+              alt=""
+              className={`${styles.img} img4`}
+              onClick={(evt) => changeImg(evt.target.src, "img4")}
+            />
+          </div>
+        </div>
+
+        <div className={styles.infoContainer}>
+          <h2>{item.name}</h2>
+          {/* <p>{item.description}</p> */}
+          <p>
+            {isExpanded
+              ? item.description
+              : `${item.description.slice(0, limit)}...`}
+          </p>
+          {item.description.length > limit && (
+            <Button
+              func={toggleReadMore}
+              text={isExpanded ? "less info" : "more info"}
+              name={styles.read}
+            ></Button>
+          )}
+
+          <h3>{item.price}</h3>
+          <div className={styles.quantityContainer}>
+            <Button text={"-"} name={styles.qtnBtn} func={decQuantity} />
+            <div>{itemQuantity}</div>
+            <Button text={"+"} name={styles.qtnBtn} func={incQuantity} />
+          </div>
+
+          <Button text={"add to cart"} name={styles.add} func={addCart} />
+
+          <div className={styles.shippingInfo}>
+            <DeliveryInfo
+              containerName={styles.deliveryContainer}
+              title={"Free Delivery"}
+              text={"Enter your Postal code for Delivery Availability"}
+              icon={TbTruckDelivery}
+            />
+            <DeliveryInfo
+              containerName={styles.deliveryContainer}
+              title={"Return Delivery"}
+              text={"Free 30 day Delivery Return"}
+              icon={TbTruckReturn}
+            />
+          </div>
         </div>
       </div>
-
-      <div className={styles.infoContainer}>
-        <h2>{item.name}</h2>
-        {/* <p>{item.description}</p> */}
-        <p>
-          {isExpanded
-            ? item.description
-            : `${item.description.slice(0, limit)}...`}
-        </p>
-        {item.description.length > limit && (
-          <Button
-            func={toggleReadMore}
-            text={isExpanded ? "Less" : "More"}
-            name={styles.read}
-          ></Button>
-        )}
-
-        <h3>${item.price}</h3>
-        <div className={styles.quantityContainer}>
-          <Button text={"-"} name={styles.qtnBtn} func={decQuantity} />
-          <div>{itemQuantity}</div>
-          <Button text={"+"} name={styles.qtnBtn} func={incQuantity} />
-        </div>
-
-        <Button text={"Add to Cart"} name={styles.add} func={addCart} />
-
-        <div className={styles.shippingInfo}>
-          <DeliveryInfo
-            containerName={styles.deliveryContainer}
-            title={"Free Delivery"}
-            text={"Enter your Postal code for Delivery Availability"}
-            icon={TbTruckDelivery}
-          />
-          <DeliveryInfo
-            containerName={styles.deliveryContainer}
-            title={"Return Delivery"}
-            text={"Free 30 day Delivery Return"}
-            icon={TbTruckReturn}
-          />
-        </div>
-      </div>
-    </div>
+      <SideScroller text="free delivery" text2="over $99 " />
+    </>
   );
 }
